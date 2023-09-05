a5e634bc1316cfceaead970311e71c694d403dc8
yihubertdev <31826788+yihubertdev@users.noreply.github.com>
2023-09-05 09:04:26 -0400

    cache firebase

--- packages/frontend/src/app/core/services/fireStore/firestore.module.ts
+++ packages/frontend/src/app/core/services/fireStore/firestore.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from "@angular/core";
-import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import {
+  enableIndexedDbPersistence,
+  getFirestore,
+  provideFirestore,
+} from "@angular/fire/firestore";
 import { PostFireStore, ArticleFireStore } from "./blog.firestore";
 import { UserService } from "./users.firestore";
 
 @NgModule({
   declarations: [],
-  imports: [provideFirestore(() => getFirestore())],
+  imports: [
+    provideFirestore(() => {
+      const db = getFirestore();
+      enableIndexedDbPersistence(db);
+      return db;
+    }),
+  ],
   providers: [UserService, PostFireStore, ArticleFireStore],
   bootstrap: [],
 })
--- packages/frontend/src/app/core/services/services.module.ts
+++ packages/frontend/src/app/core/services/services.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from "@angular/core";
 import { environment } from "src/environments/environment.dev";
 import { provideFirebaseApp, initializeApp } from "@angular/fire/app";
 import { FIREBASE_OPTIONS } from "@angular/fire/compat";
-import { getAuth, provideAuth } from "@angular/fire/auth";
 import { FireStoreServiceModule } from "./fireStore/firestore.module";
 import { FireStorageServiceModule } from "./fireStorage/fire-storage.module";
-import { BrowserStorageServiceModule } from "./browserStorage/browserStorage.module";
 import { FireAuthServiceModule } from "./fireAuth/auth.module";
 
 @NgModule({
