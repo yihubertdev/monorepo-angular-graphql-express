f11176fd5ee0f8e439230982ef043edd2dd118c7
yihubertdev <31826788+yihubertdev@users.noreply.github.com>
2023-09-07 17:29:21 -0400

    cache enable

--- packages/frontend/src/app/core/services/fireStore/firestore.module.ts
+++ packages/frontend/src/app/core/services/fireStore/firestore.module.ts
@@ -12,7 +12,20 @@ import { UserService } from "./users.firestore";
   imports: [
     provideFirestore(() => {
       const db = getFirestore();
-      enableIndexedDbPersistence(db);
+
+      enableIndexedDbPersistence(db)
+        .then(() => console.log("Enabled offline persistence"))
+        .catch((error) => {
+          if (error.code == "failed-precondition") {
+            // Multiple tabs open, persistence can only be enabled
+            // in one tab at a a time.
+            // ...
+          } else if (error.code == "unimplemented") {
+            // The current browser does not support all of the
+            // features required to enable persistence
+            // ...
+          }
+        });
       return db;
     }),
   ],
