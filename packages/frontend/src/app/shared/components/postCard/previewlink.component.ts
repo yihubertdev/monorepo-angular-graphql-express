1b4a7cfb7ade1af4874d4b78b8682d534e8969c0
yihubertdev <31826788+yihubertdev@users.noreply.github.com>
2023-09-28 13:04:40 -0400

    minor change

--- packages/frontend/src/app/shared/components/postCard/previewlink.component.ts
+++ packages/frontend/src/app/shared/components/postCard/previewlink.component.ts
@@ -1,19 +1,10 @@
 import { CommonModule } from "@angular/common";
-import { HttpClient, HttpClientModule } from "@angular/common/http";
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-} from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
 import { RouterModule } from "@angular/router";
-import { LINK_PREVIEW } from "sources-types";
 import { ILinkPreview } from "sources-types";
 @Component({
   standalone: true,
@@ -28,35 +19,38 @@ import { ILinkPreview } from "sources-types";
   selector: "preview-link-card",
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <mat-card
-      [routerLink]="preview!.url"
-      class="cursor-pointer">
-      <div
-        class="image-frame-rounded slide-image-cover-center image-height-responsive"
-        [ngStyle]="{
+    <a
+      [href]="preview!.url"
+      target="_blank"
+      class="cursor-pointer unset-tag-a">
+      <mat-card>
+        <div
+          class="image-frame-rounded slide-image-cover-center image-height-responsive"
+          [ngStyle]="{
           backgroundImage: 'url(' + preview!.image + ')',
         }"></div>
-      <mat-card-header>
-        <mat-card-title-group>
-          <mat-card-subtitle
-            ><a
-              [href]="preview!.url"
-              target="_blank"
-              >{{ preview!.url }}</a
-            ></mat-card-subtitle
-          >
-          <mat-card-title>{{ preview!.title }}</mat-card-title>
-          <!--single image display-->
-        </mat-card-title-group>
-      </mat-card-header>
-      <mat-card-content>
-        <p
-          #content
-          class="text-overflow-card"
-          [ngStyle]="{ display: '-webkit-box' }"
-          [innerHTML]="preview!.description"></p>
-      </mat-card-content>
-    </mat-card>
+        <mat-card-header>
+          <mat-card-title-group>
+            <mat-card-subtitle
+              ><a
+                [href]="preview!.url"
+                target="_blank"
+                >{{ preview!.url }}</a
+              ></mat-card-subtitle
+            >
+            <mat-card-title>{{ preview!.title }}</mat-card-title>
+            <!--single image display-->
+          </mat-card-title-group>
+        </mat-card-header>
+        <mat-card-content>
+          <p
+            #content
+            class="text-overflow-card"
+            [ngStyle]="{ display: '-webkit-box' }"
+            [innerHTML]="preview!.description"></p>
+        </mat-card-content>
+      </mat-card>
+    </a>
   `,
   styleUrls: ["./post-card.component.css"],
 })
