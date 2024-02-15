b7f475323a7cc93820166ca425cdf1bd3340ee29
yihubertdev <31826788+yihubertdev@users.noreply.github.com>
2024-02-15 18:22:30 -0500

    pub sub set up

--- packages/graphql/src/controller/user/index.ts
+++ packages/graphql/src/controller/user/index.ts
@@ -7,6 +7,10 @@ import {
 } from "../../decorators/resolver";
 import { IFaceGraphqlContext } from "../../graphql";
 import models from "../../models";
+import { PubSub } from "@google-cloud/pubsub";
+
+// Creates a client; cache this for further use
+const pubSubClient = new PubSub();
 
 @Resolver(fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"))
 class UserResolver {
@@ -22,6 +26,28 @@ class UserResolver {
       );
     }
 
+    return false;
+  }
+
+  @FieldResolver({
+    type: RESOLVER_TYPE.MUTATION,
+  })
+  async publishMessage(source, args) {
+    const { data } = args.publishMessageInput;
+
+    // Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
+    const dataBuffer = Buffer.from(data);
+    try {
+      const messageId = await pubSubClient
+        .topic("test-topic")
+        .publishMessage({data: dataBuffer});
+      console.log(`Message ${messageId} published.`);
+    } catch (error) {
+      console.error(
+        `Received error while publishing: ${(error as Error).message}`
+      );
+      process.exitCode = 1;
+    }
     return false;
   }
 
