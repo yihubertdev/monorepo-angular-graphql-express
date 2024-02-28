123ea7fdba171665d4b610d3f90c055706045509
yihubertdev <31826788+yihubertdev@users.noreply.github.com>
2024-02-28 10:33:00 -0500

    use yarn workspace

--- dist/out-tsc/frontend/src/test/unit/view/main.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-describe("MainViewComponent", () => {
-    it("should have 2", () => {
-        expect(1 + 1).toEqual(2);
-    });
-});
-//# sourceMappingURL=main.test.js.map
--- dist/out-tsc/graphql/src/database/migrations/stub.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Migrations template stub
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.up = async function (knex) { };
-exports.down = async function (knex) { };
-//# sourceMappingURL=stub.js.map
