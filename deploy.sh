d1a0fbc2028da52355d8cc9ae3e71f97bae31c66
yihubertdev <31826788+yihubertdev@users.noreply.github.com>
2024-03-17 08:25:57 -0400

    new deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 echo "packing the dependencies"
 
+sudo rm -r ./node_modules
+sudo rm -r ./packages/backend/node_modules
+sudo rm -r ./packages/frontend/node_modules
+sudo rm yarn.lock
+yarn cache clean
+
 yarn workspace type-sources pack
 
 cd ./packages/type-sources
 
 SHARED_TYPES=$(find . -type f -name "*.tgz")
+
+cp $SHARED_TYPES ../frontend/dependencies
+cp $SHARED_TYPES ../backend/dependencies
 
 yarn install
